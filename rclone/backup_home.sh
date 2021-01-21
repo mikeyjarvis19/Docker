cfab6b18c61320b8faada507f021f094de013c28
Mike <michaeljarvis95@gmail.com>
2021-01-21 23:54:39 +0000

    Add backup home shell script

---
+++ rclone/backup_home.sh
@@ -0,0 +1,9 @@
+# See https://stackoverflow.com/a/48470187/7752249
+rc_pass="mypass"
+json_fmt='{"main": {"BackupDir": "encrypted_gdrive_1:archive/home_synced/home_synced_%s"}}'
+json_str=$(printf "$json_fmt" "$(date +%d_%m_%Y)")
+docker exec -d rclone rclone rc options/set --json $json_str --rc-user=pi --rc-pass=$rc_pass
+#options=$(docker exec rclone rclone rc options/get --rc-user=pi --rc-pass=$rc_pass)
+#echo $options
+docker exec -d rclone rclone rc sync/sync srcFs="/data/synced" dstFs=encrypted_gdrive_1:home_synced --rc-user=pi --rc-pass=$rc_pass -async
+docker exec -d rclone rclone rc options/set --json '{"main": {"BackupDir": ""}}' --rc-user=pi --rc-pass=$rc_pass

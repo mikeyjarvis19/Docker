478b4279d8c42c5ff45d6a249d31c0bca2c124e6
Mike <michaeljarvis95@gmail.com>
2021-01-22 19:43:56 +0000

    Pass sync args as JSON to prevent async flag being ignored

--- rclone/backup_home.sh
+++ rclone/backup_home.sh
@@ -5,5 +5,5 @@ json_str=$(printf "$json_fmt" "$(date +%d_%m_%Y)")
 docker exec rclone rclone rc options/set --json "$json_str" --rc-user=$rc_user --rc-pass=$rc_pass
 #options=$(docker exec rclone rclone rc options/get --rc-user=$rc_user --rc-pass=$rc_pass)
 #echo $options
-docker exec rclone rclone rc sync/sync srcFs="/data/synced" dstFs=encrypted_gdrive_1:home_synced --rc-user=$rc_user --rc-pass=$rc_pass -async
-docker exec -d rclone rclone rc options/set --json '{"main": {"BackupDir": ""}}' --rc-user=$rc_user --rc-pass=$rc_pass
+docker exec rclone rclone rc sync/sync --json '{"srcFs": "/data/synced", "dstFs": "encrypted_gdrive_1:home_synced", "_async": true}' --rc-user=$rc_user --rc-pass=$rc_pass
+#docker exec -d rclone rclone rc options/set --json '{"main": {"BackupDir": ""}}' --rc-user=$rc_user --rc-pass=$rc_pass

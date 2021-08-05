0bf18b61d690666f792e7b414bb70763e6dffc93
Mike <michaeljarvis95@gmail.com>
2021-08-05 11:59:07 +0100

    Add `dockerupdates.sh`

---
+++ dockerupdates.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ 
+# Declare a string array with type
+declare -a StringArray=("gluetun" "transmission" "jackett" "sabnzbd" 
+"sonarr" "radarr" "portainer" "caddy" "ddclient" "linuxserver-nextcloud"
+"ombi" "rclone" "tautulli" "diun")
+ 
+# Read the array values with space
+for val in "${StringArray[@]}"; do
+  echo "Updating container for $val"
+  cd /home/pi/Docker/$val
+  docker-compose pull && docker-compose up -d --remove-orphans
+done
